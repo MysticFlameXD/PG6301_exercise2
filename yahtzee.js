37940e000fcef3b8a61f345073e7328befc75b5b
skogholtsteinar <skogholtsteinar@gmail.com>
2026-08-27 15:19:04 +0200

    Ones

--- yahtzee.js
+++ yahtzee.js
@@ -8,8 +8,8 @@ export function yahtzeeScore(scoreName, diceNumbers) {
   } else if (scoreName === "Ones") {
     let total = 0;
     diceNumbers.forEach((dice) => {
-      if (dice === 1){
-        total+= dice;
+      if (dice === 1) {
+        total += dice;
       }
     });
     return total;
